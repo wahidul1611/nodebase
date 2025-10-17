079aaecbdda245c5ee57b3a2870c1e4d459b2d71
wahidul1611 <wahidulislam1611@gmail.com>
2025-10-17 13:40:53 +0600

    06: background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,30 @@
-import { requireAuth } from "@/lib/auth-utils"
-import { caller } from "@/trpc/server"
-import { LogoutButton } from "./logout"
+"use client"
 
-const Page = async  () => {
-    await requireAuth()
+import { Button } from "@/components/ui/button"
+import { LogoutButton } from "./logout"
+import { useTRPC } from "@/trpc/client"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { toast } from "sonner"
+
+const Page = () => {
+    const trpc = useTRPC()
+    const queryClient = useQueryClient()
+    const { data } = useQuery(trpc.getWorkflows.queryOptions())
+
+    const create = useMutation(trpc.createWorkflow.mutationOptions({
+        onSuccess: () => {
+            toast.success("Job queued")
+        }
+    }))
     
-    const data = await caller.getUsers()
     return (
         <div className="min-h-screen min-w-scren flex items-center justify-center flex-col gap-y-6">
             protected server component
-            {JSON.stringify(data)}
+            {JSON.stringify(data, null, 2)}
             <div>
+                <Button disabled={create.isPending} onClick={() => create.mutate()}>
+                    Create Workflow
+                </Button>
                 <LogoutButton />
             </div>
         </div>
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,16 +1,20 @@
-
-import { createTRPCRouter, protectedProcedure } from '../init';
-import prisma from '@/lib/db';
+import { inngest } from "@/inngest/client";
+import { createTRPCRouter, protectedProcedure } from "../init";
+import prisma from "@/lib/db";
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    
+    getWorkflows: protectedProcedure.query(({ ctx }) => {
+        return prisma.workflow.findMany()
+    }),
+    createWorkflow: protectedProcedure.mutation( async () => {
+        await inngest.send({
+            name: "test/hello.world",
+            data: {
+                email: "antonio@email.com"
+            }
+        })
 
-    return prisma.user.findMany({
-      where: {
-        id: ctx.auth.user.id
-      }
-    })
-  })
+        return { success: true, message: "Job queued"}
+    }) 
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
